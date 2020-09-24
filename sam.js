5cae295a4a796a6880837301348f0feb32b49d8d
SeojinK1m <62783136+SeojinK1m@users.noreply.github.com>
2020-09-23 20:52:21 -0500

    Add files via upload

---
+++ sam.js
@@ -0,0 +1,36 @@
+function c(){
+    alert("The C Programming Language")
+}
+function python(){
+    alert("The Python Programming Language")
+}
+function java(){
+    alert("The Java Programming Language")
+}
+function js(){
+    alert("The JavaScript Scripting Language")
+}
+
+function scrollMe(){
+    document.querySelector("body").scrollIntoView({behavior: "smooth"});
+}
+
+function scrollSkill(){
+    document.querySelector("#jumpSkill").scrollIntoView({behavior: "smooth"});
+}
+
+function scrollExp(){
+    document.querySelector("#jumpExp").scrollIntoView({behavior: "smooth"});
+}
+
+function openLI(){
+    window.open("https://www.linkedin.com/in/seojin-kim-35080918a/");
+}
+
+function openGH(){
+    window.open("https://github.com/SeojinK1m");
+}
+
+function openResume(){
+    window.open("Sam Kim Resume.pdf")
+}
